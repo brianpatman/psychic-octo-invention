0087044294b441a261a3253ce896876e730fddb2
Brian Patman <Black_Reaper_Hei@yahoo.com>
2023-12-21 14:44:34 -0600

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,35 +8,34 @@ const links = [
 
 export default function Layout({children}: {children: React.ReactNode}){
 	return (
-		<>
-			<div className="flex flex-col md:flex-row md:overflow-hidden">
-				<div classNane="w-full flex-none md:w-64">
-					<div className="sidenav">
-						<nav>
-							<ul>
-								{
-								links.map((link) => {
-									return (
-										<li>
-											<Link
-												key={link.name} 
-												href={link.href} 
-												className="">
-												<p>{link.name}</p>		
-											</Link>
-										</li>
-									)
-								})
-								}
-							</ul>
-						</nav>
-					</div>
-				</div>
-
-				<div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-					{children}
+		<div className="flex flex-col md:flex-row md:overflow-hidden">
+			<div classNane="w-full flex-none md:w-64">
+				<div className="sidenav">
+					<nav>
+						<ul>
+							{
+							links.map((link) => {
+								return (
+									<li>
+										<Link
+											key={link.name} 
+											href={link.href} 
+											className="">
+											<p>{link.name}</p>		
+										</Link>
+									</li>
+								);
+							})
+							}
+						</ul>
+					</nav>
 				</div>
 			</div>
-		</>
+
+			<div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+				{children}
+			</div>
+		</div>
+
 	);
 }
