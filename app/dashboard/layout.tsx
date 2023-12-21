945a4fd31660ce7f6883983bc0cd6c0227fcdc63
Brian Patman <Black_Reaper_Hei@yahoo.com>
2023-12-21 13:46:07 -0600

    Update layout.tsx
    Turns out you need to specify in a layout.tsx where the children nodes should go!

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,30 @@
 export default function Layout({children}: {children: React.ReactNode}){
 	return (
-		<div className="sidenav">
-			<nav>
-				<ul>
-				    <li className="rounded-sm bg-emerald-300 mb-2">
-				    	<a href="">Home</a>
-				    </li>
-					
-					<li className="rounded-sm bg-emerald-300 mb-2">
-						<a href="">Invoices</a>
-					</li>
+		<div className="flex flex-col md:flex-row md:overflow-hidden">
+			<div classNanem="w-full flex-none md:w-64">
+				<div className="sidenav">
+					<nav>
+						<ul>
+						    <li className="rounded-sm bg-emerald-300 mb-2">
+						    	<a href="">Home</a>
+						    </li>
+							
+							<li className="rounded-sm bg-emerald-300 mb-2">
+								<a href="">Invoices</a>
+							</li>
 
-					<li className="rounded-sm bg-emerald-300 mb-2">
-						<a href="">Customers</a>
-					</li>
-				</ul>
-			</nav>
+							<li className="rounded-sm bg-emerald-300 mb-2">
+								<a href="">Customers</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+
+			<div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+				{children}
+			</div>
 		</div>
+
 	);
 }
