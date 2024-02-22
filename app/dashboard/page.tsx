5feacc158c480b90c64512ab45915b975acff7da
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-02-22 16:14:14 -0600

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,6 +9,10 @@ export default function Page(){
 	const [number2,setNum2] = useState(0);
 	const total = number1 + number2;
 
+	if(isNaN(total)){
+		total = "";
+	}
+
 	return <>
 		<h1 className="text-sky-400 uppercase text-lg">Dashboard Page</h1>
 		<label>
@@ -16,7 +20,7 @@ export default function Page(){
 			<input 
 				id="first-num" 
 				type="number"
-				className="text-black p-10 rounded-md" 
+				className="text-black p-4 rounded-md" 
 				name="first_number"
 				value={number1}
 				onChange={(event) => setNum1( parseInt(event.target.value))}
@@ -28,16 +32,12 @@ export default function Page(){
 			<input 
 				id="second-num" 
 				type="number"
-				className="text-black p-10 rounded-md" 
+				className="text-black p-4 rounded-md" 
 				name="second_number"
 				value={number2} 
 				onChange={(event) => setNum2( parseInt(event.target.value))}
 			/>
 		</label>
-
-		<button>
-			Click Me
-		</button>
 
 		<div className="result">
 			<h2>{total}</h2>
