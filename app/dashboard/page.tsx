836342630d439abbd6f4c47a38e6b6c40b7c141a
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-02-22 15:25:12 -0600

    Update page.tsx
    Adding a form to the Dashboard page to test out State.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,36 @@
 import "@/app/globals.css";
 
 export default function Page(){
-	return <h1>Dashboard Page</h1>;
+	const [total,setTotal] = useState(0);
+
+	function handleClick(){
+		// alert("clicked!");
+
+		var FirstNum = parseInt(document.querySelector("#first-num").value());
+		var SecondNum = parseInt(document.querySelector("#second-num").value());
+
+		var newTotal= FirstNum + SecondNum;
+		setTotal(newTotal);
+	}
+
+	return <>
+		<h1 className="text-sky-400 uppercase text-lg">Dashboard Page</h1>
+		<label>
+			First Number
+			<input id="first-num" type="number" name="first_number"/>
+		</label>
+
+		<label>
+			Second Number
+			<input id="second-num" type="number" name="second_number"/>
+		</label>
+
+		<button onClick={handleClick}>
+			Click Me
+		</button>
+
+		<div class="result">
+			<h2>{total}</h2>
+		</div>
+	</>;
 }
