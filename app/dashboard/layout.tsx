fe10ba3acedf53c0bd0f32cbee0815eadf22f93c
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-02-23 16:12:33 -0600

    Update layout.tsx
    Updated Menu to contain this new page.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,24 +4,23 @@ import "@/app/globals.css";
 
 const links = [
 	{id:"1", name:"Home", href:"/dashboard"},
-	{id:"2", name:"Simple Addition", href:"/dashboard/simple_add"}
-	// {id:"2", name:"Invoices", href:"/dashboard/invoices"},
-	// {id:"3", name:"Customers", href:"/dashboard/customers"},
+	{id:"2", name:"Simple Addition", href:"/dashboard/simple_add"},
+	{id:"3", name:"Simple Add w/ Single State", href:"/dashboard/simple_add_single_state"}
 ]
 
 export default function Layout({children}: {children: React.ReactNode}){
 	return (
 		<div className="flex flex-col md:flex-row md:overflow-hidden">
-			<div className="w-full flex-none md:w-64">
+			<div className="w-full flex-none md:w-48 height-screen bg-teal-700">
 				<div className="sidenav">
 					<nav>
 						<ul>
 							{
 								links.map(link => 
-									<li key={link.id} className="block">
+									<li key={link.id} className="block border-b-1 border-white block p-5 bg-teal-500">
 										<Link
 											href={link.href} 
-											className={`${roboto.className} block p-5 bg-teal-500 text-white hover:bg-teal-700`}>
+											className={`${roboto.className} text-white hover:bg-teal-700`}>
 											{link.name}	
 										</Link>
 									</li>
