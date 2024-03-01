3696790751087eedefa5631b2bdef7cad53bd9af
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-03-01 15:32:11 -0600

    Update ToDo List Item
    Added input element and state to keep track of user checks.

--- app/dashboard/todo_list_item.tsx
+++ app/dashboard/todo_list_item.tsx
@@ -1,6 +1,17 @@
+'use client';
+import { useState } from "react";
+
 export default function ToDoListItem(){
+	const [checked,setCheck] = useState(false);
+
+	function handleCheck(ev){
+		setCheck(!checked);
+	}
 	
 	return <> 
-		<p>ToDoListItem</p>
+		<label className={{ checked ? "line-through" : "" }}>
+			<input type="checkbox" onChange={this.handleCheck} />
+			Laundry
+		</label>
 	</>;
 }
