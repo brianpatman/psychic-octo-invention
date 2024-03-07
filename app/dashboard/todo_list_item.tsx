e743bb05a7e57b7fe826902c85ab19fa3451def6
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-03-07 17:31:07 -0600

    Update todo_list_item.tsx
    Basically testing out what this internal Item component would look like if I did decide to put all associated functionality within it.

--- app/dashboard/todo_list_item.tsx
+++ app/dashboard/todo_list_item.tsx
@@ -2,17 +2,23 @@
 import { useState } from "react";
 
 function Item({name, inEditMode} : {name:string,inEditMode:boolean}){
+	const [checked,setCheck] = useState(false);
 	const [itemName,setName] = useState( name );
 
 	if(inEditMode){
 		return <>
-			<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
+			<label>
+				<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
+				<span className="sr-only">Edit {itemName} field</span>
+			</label>
 		</>;
 	}
 	else{
 		return <>
-			<input className="mr-1.5" type="checkbox" />
-			<span className="itemName">{itemName}</span>
+			<label className={checked ? "line-through" : "" }>
+				<input className="mr-1.5" type="checkbox" />
+				<span className="itemName">{itemName}</span>
+			</label>
 		</>
 	}
 }
@@ -32,19 +38,22 @@ export default function ToDoListItem({ name } : {name:string}){
 
 	return <> 
 		<div className="flex gap-24 items-center justify-between border-y border-white py-2 px-3">
-			<label className={checked ? "line-through" : "" }>
 				{ editable ? (
 					<>
-						<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
+						<label>
+							<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
+							<span className="sr-only">Edit {itemName} field</span>
+						</label>
 					</>
 				) : (
 					<>
-						<input className="mr-1.5" type="checkbox" onChange={handleCheck} />
-						<span className="itemName">{itemName}</span>
+						<label className={checked ? "line-through" : "" }>
+							<input className="mr-1.5" type="checkbox" onChange={handleCheck} />
+							<span className="itemName">{itemName}</span>
+						</label>
 					</>
 				)}
-				<Item name={name} inEditMode={editable} />
-			</label>
+			<Item name={name} inEditMode={editable} />
 			<button onClick={editItem}>
 				{ editable ? "Save Item" : "Edit Item"}
 			</button>
