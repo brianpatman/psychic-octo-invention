6c9ce93ded266b649503bafac59cbc20705cc8c6
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-03-08 10:00:24 -0600

    Update todo_list_item.tsx
    So I successfully did put part of the ToDo List item into another internal component, but I think that component is a little too small to warrant being a whole other component.
    
    Commented out that checklistItem component, and integrated that code back into the ToDo List Item.

--- app/dashboard/todo_list_item.tsx
+++ app/dashboard/todo_list_item.tsx
@@ -1,35 +1,35 @@
 'use client';
 import { useState } from "react";
 
-function Item({name, inEditMode} : {name:string,inEditMode:boolean}){
-	const [checked,setCheck] = useState(false);
-	const [itemName,setName] = useState( name );
+// function checklistItem({name, inEditMode} : {name:string,inEditMode:boolean}){
+// 	const [checked,setCheck] = useState(false);
+// 	const [itemName,setName] = useState( name );
 
-	function handleCheck(){
-		setCheck(checked => !checked);
-	}
+// 	function handleCheck(){
+// 		setCheck(checked => !checked);
+// 	}
 
-	if(inEditMode){
-		return <>
-			<label>
-				<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
-				<span className="sr-only">Edit {itemName} field</span>
-			</label>
-		</>;
-	}
-	else{
-		return <>
-			<label className={checked ? "line-through" : "" }>
-				<input className="mr-1.5" type="checkbox" onChange={handleCheck} />
-				<span className="itemName">{itemName}</span>
-			</label>
-		</>
-	}
-}
+// 	if(inEditMode){
+// 		return <>
+// 			<label>
+// 				<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
+// 				<span className="sr-only">Edit field</span>
+// 			</label>
+// 		</>;
+// 	}
+// 	else{
+// 		return <>
+// 			<label className={checked ? "line-through" : "" }>
+// 				<input className="mr-1.5" type="checkbox" onChange={handleCheck} />
+// 				<span className="itemName">{itemName}</span>
+// 			</label>
+// 		</>
+// 	}
+// }
 
 export default function ToDoListItem({ name } : {name:string}){
-	const [checked,setCheck] = useState(false);
 	const [editable,setEditable] = useState(false);
+	const [checked,setCheck] = useState(false);
 	const [itemName,setName] = useState( name );
 
 	function handleCheck(){
@@ -57,7 +57,7 @@ export default function ToDoListItem({ name } : {name:string}){
 					</label>
 				</>
 			)}
-			<Item name={name} inEditMode={editable} />
+			{/*<checklistItem name={name} inEditMode={editable} />*/}
 			<button onClick={editItem}>
 				{ editable ? "Save Item" : "Edit Item"}
 			</button>
