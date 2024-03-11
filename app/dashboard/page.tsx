4d65dbe09dbe61742409016066c791f7cc600b94
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-03-11 17:18:32 -0500

    Update page.tsx
    Added ToDo List component instead of individual list items.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,11 +7,10 @@ export default function Page(){
 	return <>
 		<h1 className="text-sky-400 uppercase text-lg mb-4">Dashboard</h1>
 
-		<div className="block">
-			<ToDoListItem name="Laundry"/>
+		<ToDoList></ToDoList>
+		{/*	<ToDoListItem name="Laundry"/>
 			<ToDoListItem name="Groceries"/>
-			<ToDoListItem name="Get Federal Income Taxes Done"/>
-		</div>
+			<ToDoListItem name="Get Federal Income Taxes Done"/>*/}
 
 		<h2 className="mt-10">React Apps to Make</h2>
 		<ul className="list-disc list-inside">
