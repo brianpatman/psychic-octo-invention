5a8371a8e15a334abba2875ad24e0a00e4566546
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-03-28 13:41:34 -0500

    Update todo_list_item.tsx
    Trying alternate way to do this.

--- app/dashboard/todo_list_item.tsx
+++ app/dashboard/todo_list_item.tsx
@@ -99,30 +99,30 @@ function ToDoListItem({name:string, showCompleted:bool}){
 		setEditable(editable => !editable);
 	}
 
-	return <> 
-		{ (!showCompleted && !checked) &&
-		(
-			<div className="flex gap-24 items-center justify-between border-y border-white py-2 px-3">
-				{ editable ? (
-					<>
-						<label>
-							<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
-							<span className="sr-only">Edit {itemName} field</span>
-						</label>
-					</>
-				) : (
-					<>
-						<label className={checked ? "line-through" : "" }>
-							<input className="mr-1.5" type="checkbox" onChange={handleCheck} />
-							<span className="itemName">{itemName}</span>
-						</label>
-					</>
-				)}
-				{/*<checklistItem name={name} inEditMode={editable} />*/}
-				<button onClick={editItem}>
-					{ editable ? "Save Item" : "Edit Item"}
-				</button>
-			</div>
-		)}
-	</>;
+	if(!showCompleted || !checked){
+		return <> 
+				<div className="flex gap-24 items-center justify-between border-y border-white py-2 px-3">
+					{ editable ? (
+						<>
+							<label>
+								<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
+								<span className="sr-only">Edit {itemName} field</span>
+							</label>
+						</>
+					) : (
+						<>
+							<label className={checked ? "line-through" : "" }>
+								<input className="mr-1.5" type="checkbox" onChange={handleCheck} />
+								<span className="itemName">{itemName}</span>
+							</label>
+						</>
+					)}
+					{/*<checklistItem name={name} inEditMode={editable} />*/}
+					<button onClick={editItem}>
+						{ editable ? "Save Item" : "Edit Item"}
+					</button>
+				</div>
+		</>;
+	}
+
 }
