f7ff08881881d02a023549b50951189e114e20e1
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-05-03 15:14:59 -0500

    Update todo_list_item.tsx

--- app/dashboard/todo_list_item.tsx
+++ app/dashboard/todo_list_item.tsx
@@ -106,7 +106,7 @@ function ToDoListItem({ name,showCompleted,ToDos,setToDos }:{name:string,showCom
 	}
 
 	function deleteItem(){
-		setToDos( ToDos.filter( listItem => listItem.name != itemName) );
+		setToDos( ToDos.filter( (listItem) => listItem.name != itemName) );
 	}
 
 	if( !checked || (checked && showCompleted)){
