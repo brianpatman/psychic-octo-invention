a1d4eaf286dbe9fe1c40c75907976d5c8a3bdd5d
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-05-07 17:24:35 -0500

    Create page.tsx
    Creating Page to output ToDo List component.

---
+++ app/dashboard/todo/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import "@/app/globals.css";
+import { useState } from 'react';
+import ToDoList from "./todo_list";
+
+export default function ToDoList(){
+	return <>
+		<h1 className="text-sky-400 uppercase text-lg mb-4">To-Do List</h1>
+		<ToDoList></ToDoList>
+	</>;
+}
