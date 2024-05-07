a83525946ab52d29e3d20982c09277549be08abf
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-05-07 17:27:58 -0500

    Update page.tsx
    Fixed duplicate reference.

--- app/dashboard/todo/page.tsx
+++ app/dashboard/todo/page.tsx
@@ -3,7 +3,7 @@ import "@/app/globals.css";
 import { useState } from 'react';
 import ToDoList from "./todo_list";
 
-export default function ToDoList(){
+export default function ToDoPage(){
 	return <>
 		<h1 className="text-sky-400 uppercase text-lg mb-4">To-Do List</h1>
 		<ToDoList></ToDoList>
