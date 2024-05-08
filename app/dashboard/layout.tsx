ed4fa3f2223e4501cb9779c5306d002a6de28c39
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-05-08 09:53:14 -0500

    Update layout.tsx
    Added some additional styling and re-ordered nav links.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,15 +4,15 @@ import "@/app/globals.css";
 
 const links = [
 	{id:"1", name:"Home", href:"/dashboard"},
-	{id:"2", name:"Simple Addition", href:"/dashboard/simple_add"},
+	{id:"4", name:"To-Do List", href="/dashboard/todo"},
 	{id:"3", name:"Simple Add w/ Single State", href:"/dashboard/simple_add_single_state"},
-	{id:"4", name:"To-Do List", href:"/dashboard/todo"}
+	{id:"2", name:"Simple Addition", href:"/dashboard/simple_add"},
 ]
 
 export default function Layout({children}: {children: React.ReactNode}){
 	return (
 		<div className="flex flex-col md:flex-row md:overflow-hidden">
-			<div className="w-full flex-none w-48 md:w-64 height-screen bg-teal-700">
+			<div className="w-full flex-none w-48 md:w-64 h-screen bg-teal-700">
 				<div className="sidenav">
 					<nav>
 						<ul>
