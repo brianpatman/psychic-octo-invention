69fb2c6d508b38b3fad580af67ca42cfb6d3f02f
Brian Patman <Black_Reaper_Hei@yahoo.com>
2024-05-16 16:44:17 -0500

    Update ToDoListItem.tsx
    Cleaned up old code in the ToDoListItem component and moved a lot of multiple tailwind class styles to the css module.

--- app/dashboard/todo/ToDoListItem.tsx
+++ app/dashboard/todo/ToDoListItem.tsx
@@ -3,32 +3,6 @@ import { useRef, useState } from "react";
 import styles from "./ToDoListItem.module.css";
 import { TrashIcon, EditIcon, SaveIcon } from "@/app/ui/icons";
 
-// function checklistItem({name, inEditMode} : {name:string,inEditMode:boolean}){
-// 	const [checked,setCheck] = useState(false);
-// 	const [itemName,setName] = useState( name );
-
-// 	function handleCheck(){
-// 		setCheck(checked => !checked);
-// 	}
-
-// 	if(inEditMode){
-// 		return <>
-// 			<label>
-// 				<input type="text" className="edit-name text-black" onChange={(event) => setName(event.target.value)} value={itemName} />
-// 				<span className="sr-only">Edit field</span>
-// 			</label>
-// 		</>;
-// 	}
-// 	else{
-// 		return <>
-// 			<label className={checked ? "line-through" : "" }>
-// 				<input className="mr-1.5" type="checkbox" onChange={handleCheck} />
-// 				<span className="itemName">{itemName}</span>
-// 			</label>
-// 		</>
-// 	}
-// }
-
 export default function ToDoListItem({ id,name,showCompleted,ToDos,setToDos,setAriaLive }:{id:any,name:string,showCompleted:boolean,ToDos:Array<any>,setToDos:any,setAriaLive:any}){
 	const [editable,setEditable] = useState(false);
 	const [checked,setCheck] = useState(false);
@@ -58,18 +32,28 @@ export default function ToDoListItem({ id,name,showCompleted,ToDos,setToDos,setA
 
 	if( !checked || (checked && showCompleted)){
 		return <> 
-				<div className="flex gap-24 items-center justify-between border-y border-white py-2 px-3">
+				<li className={styles.listItem}>
 					{ editable ? (
 						<>
 							<label>
-								<input type="text" className={styles['edit-name']} onChange={(event) => setName(event.target.value)} value={itemName} />
+								<input 
+									type="text" 
+									className={styles.editName} 
+									onChange={(event) => setName(event.target.value)} 
+									value={itemName} 
+								/>
 								<span className="sr-only">Edit {itemName} field</span>
 							</label>
 						</>
 					) : (
 						<>
 							<label className={checked ? "line-through" : "" }>
-								<input className="mr-1.5" type="checkbox" onChange={handleCheck} checked={checked} />
+								<input 
+									className="mr-1.5" 
+									type="checkbox" 
+									onChange={handleCheck} 
+									checked={checked} 
+								/>
 								<span className="itemName">{itemName}</span>
 							</label>
 						</>
@@ -84,7 +68,7 @@ export default function ToDoListItem({ id,name,showCompleted,ToDos,setToDos,setA
 							<TrashIcon></TrashIcon>
 						</button>
 					</div>
-				</div>
+				</li>
 		</>;
 	}
 
